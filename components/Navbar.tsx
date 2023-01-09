4543fb034066edb7819fd99a7dfdd0b3ae8bea73
conno <25connorgriffin@gmail.com>
2023-01-08 16:12:08 -0800

    starting layout

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,29 +2,29 @@ import { JSX } from "preact";
 
 
 export default function NavigationBar(
-    props: { active: string; class?: string },
+    props: { active: string; class?: string; },
   ) {
     const items = [
       {
-        name: "Random",
-        href: "/asdf",
+        name: "Home",
+        href: "/",
       },
       {
         name: "2",
-        href: "/",
+        href: "/asdf",
       }
     ];
 
     return (
-      <nav class='h-full bg-red-900 w-1/8'>
-        <ul class=" mt-20 text-white flex items-center gap-4 flex-col">
+      <nav class='h-full bg-[#07031f] w-1/8'>
+        <ul class=" mt-6 p-6 text-[#fffeeb] flex items-center gap-12 flex-col">
           {items.map((item) => (
 
             <li>
               <a
                 href={item.href}
-                class={`p-2 hover:underline text-[#fbffc9]
-                text-lg
+                class={`p-2 hover:underline text-[#fffdd9]
+                text-2xl
                 ${ props.active == item.href ? "font-bold" : ""}
                   `}
               >
--- routes/index.tsx
+++ routes/index.tsx
@@ -4,19 +4,27 @@ import Navbar from "../components/Navbar.tsx";
 
 export default function Home() {
   return (
-    <div class='w-full h-screen bg-[#0a011a] flex text-[#fefff7] '>
+    <div class='w-full h-screen bg-[#00072D] flex text-[#fffdd9] '>
       <Head>
         <title>Fresh App</title>
       </Head>
-      <Navbar title="" active="/" />
-      <div class="p-4 mx-auto max-w-screen-lg">
-
-        <p class="my-6">
-          Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
-          file, and refresh.
-        </p>
-        <Counter start={3} />
+      {/*max-w-screen-lg*/}
+      <Navbar  />
+      <div class="p-4 bg-red-200 bg-opacity-20 flex flex-wrap mx-auto w-3/4">
+    <div class="my-6 h-10 w-1/2 text-4xl">
+        <h1 >
+          Some sort of title
+        </h1>
+        <h3 class="mt-5 text-lg"> description asdfasadfasdfasdfasdfasdfasdf 
+        asdfasdfsadf asdfasdf asdfasd fasdf asdfsadf asdfasdfasdfasdf asdf
+        sdfasdfasdfasdfasdfasdfasdfasdfasdfasdf</h3> 
+        </div>
+        <div class='bg-blue-900 w-1/2 h-1/4'> add something like logo idk</div>
+       
+        
+        
       </div>
+      
     </div>
   );
 }

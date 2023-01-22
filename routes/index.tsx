84c9eb6a63369f8d1a9f6ecb766af6ca36306442
conno <25connorgriffin@gmail.com>
2023-01-21 17:32:08 -0800

    links

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,6 +1,7 @@
 import { Head } from "$fresh/runtime.ts";
 import Counter from "../islands/Counter.tsx";
 import Navbar from "../components/Navbar.tsx";
+import Links from "../components/links.tsx";
 
 export default function Home() {
   return (
@@ -10,12 +11,13 @@ export default function Home() {
       </Head>
       {/*max-w-screen-lg*/}
       <Navbar  />
-      <div class="p-4 gap-10 bg-red-200 bg-opacity-20 flex flex-wrap mx-auto w-3/4">
+      <div class="p-4 gap-10 bg-opacity-20 flex flex-wrap mx-auto w-3/4">
     <div class="my-6 h-10 w-5/12 text-4xl">
         <h1 >
           Connor Griffin 
         </h1>
-        <h3 class="mt-5 text-lg"> I do the school and the code and the yes</h3> 
+        <Links />
+        <h3 class="mt-5 text-lg"> I do the school and the yes</h3> 
         </div>
         <div class='bg-blue-900 w-5/12 h-1/4'> add something like logo idk</div>
        
